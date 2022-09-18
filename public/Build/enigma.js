e89b4ca426b803a0698c38aeca6b7b1c89c66647
Mason Spong <mason.spong@gmail.com>
2022-09-18 16:11:55 -0600

    Finish plugboard key press logic

--- public/Build/enigma.js
+++ public/Build/enigma.js
@@ -60,8 +60,7 @@ class WireMap {
         return this.map.indexOf(letter);
     }
     swap(letter1, letter2) {
-        this.map[letter1] = letter2;
-        this.map[letter2] = letter1;
+        [this.map[letter1], this.map[letter2]] = [this.map[letter2], this.map[letter1]];
     }
     isValidMapString(mapString) {
         let lettersKeys = Object.keys(Letters).filter((key) => isNaN(Number(key))); // Disregard reverse enum map num -> key
@@ -420,15 +419,20 @@ class PlugboardController {
             if (this.model.plugboard.currentSelection !== null) {
                 // This is second click
                 console.log("second click");
-                if (this.model.plugboard.wireMap.getAtIdx(letter) !== letter) {
-                    // The clicked key has previously been swapped
-                }
-                else {
+                if (this.model.plugboard.wireMap.getAtIdx(letter) === letter) {
                     // The clicked key has not been swapped
-                    this.model.plugboard.wireMap.swap(this.model.plugboard.currentSelection, letter);
-                    this.view.setColor(lettersToChar(letter), this.plugboardColors[this.colorIdx]);
-                    this.colorIdx = (this.colorIdx + 1) % this.plugboardColors.length;
-                    this.model.plugboard.currentSelection = null;
+                    if (this.model.plugboard.wireMap.getAtIdx(letter) === this.model.plugboard.currentSelection) {
+                        // The second clicked key is the same as the first clicked key
+                        this.model.plugboard.currentSelection = null;
+                        this.view.setColor(lettersToChar(letter), this.backgroundColor);
+                    }
+                    else {
+                        // The second clicked key is different than the first clicked key
+                        this.model.plugboard.wireMap.swap(this.model.plugboard.currentSelection, letter);
+                        this.view.setColor(lettersToChar(letter), this.plugboardColors[this.colorIdx]);
+                        this.colorIdx = (this.colorIdx + 1) % this.plugboardColors.length;
+                        this.model.plugboard.currentSelection = null;
+                    }
                 }
             }
             else {
@@ -436,7 +440,9 @@ class PlugboardController {
                 console.log("first click");
                 if (this.model.plugboard.wireMap.getAtIdx(letter) !== letter) {
                     // the clicked key has been swapped
-                    this.model.plugboard.wireMap;
+                    this.view.setColor(lettersToChar(letter), this.backgroundColor);
+                    this.view.setColor(lettersToChar(this.model.plugboard.wireMap.getAtIdx(letter)), this.backgroundColor);
+                    this.model.plugboard.wireMap.swap(letter, this.model.plugboard.wireMap.getAtIdx(letter));
                 }
                 else {
                     // the clicked key has not been swapped
--- src/enigma.ts
+++ src/enigma.ts
@@ -69,8 +69,7 @@ class WireMap {
   }
 
   swap(letter1: Letters, letter2: Letters) {
-    this.map[letter1] = letter2;
-    this.map[letter2] = letter1;
+    [this.map[letter1], this.map[letter2]] = [this.map[letter2], this.map[letter1]]
   }
 
   private isValidMapString(mapString: string): boolean {
@@ -575,23 +574,28 @@ class PlugboardController {
     if (this.model.plugboard.currentSelection !== null) {
       // This is second click
       console.log("second click")
-      if (this.model.plugboard.wireMap.getAtIdx(letter) !== letter) {
-        // The clicked key has previously been swapped
-
-      } else {
+      if (this.model.plugboard.wireMap.getAtIdx(letter) === letter) {
         // The clicked key has not been swapped
-        this.model.plugboard.wireMap.swap(this.model.plugboard.currentSelection, letter);
-        this.view.setColor(lettersToChar(letter), this.plugboardColors[this.colorIdx]);
-        this.colorIdx = (this.colorIdx + 1) % this.plugboardColors.length
-        this.model.plugboard.currentSelection = null;
-
+        if (this.model.plugboard.wireMap.getAtIdx(letter) === this.model.plugboard.currentSelection) {
+          // The second clicked key is the same as the first clicked key
+          this.model.plugboard.currentSelection = null;
+          this.view.setColor(lettersToChar(letter), this.backgroundColor);
+        } else {
+          // The second clicked key is different than the first clicked key
+          this.model.plugboard.wireMap.swap(this.model.plugboard.currentSelection, letter);
+          this.view.setColor(lettersToChar(letter), this.plugboardColors[this.colorIdx]);
+          this.colorIdx = (this.colorIdx + 1) % this.plugboardColors.length
+          this.model.plugboard.currentSelection = null;
+        }
       }
     } else {
       // This is the first click
       console.log("first click");
       if (this.model.plugboard.wireMap.getAtIdx(letter) !== letter) {
         // the clicked key has been swapped
-        this.model.plugboard.wireMap
+        this.view.setColor(lettersToChar(letter), this.backgroundColor);
+        this.view.setColor(lettersToChar(this.model.plugboard.wireMap.getAtIdx(letter)), this.backgroundColor)
+        this.model.plugboard.wireMap.swap(letter, this.model.plugboard.wireMap.getAtIdx(letter));
       } else {
         // the clicked key has not been swapped
         this.model.plugboard.currentSelection = letter;
